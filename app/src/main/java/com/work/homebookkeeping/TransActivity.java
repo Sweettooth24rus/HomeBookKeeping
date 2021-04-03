98887798fb2b4a214ab1884eb4e19c1729e9eed5
Konstantin <Sweettooth24rus>
2021-04-03 19:37:14 +0700

    Jumps

--- app/src/main/java/com/work/homebookkeeping/TransActivity.java
+++ app/src/main/java/com/work/homebookkeeping/TransActivity.java
@@ -1,14 +1,28 @@
 package com.work.homebookkeeping;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TransActivity extends AppCompatActivity {
 
+    Button add;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trans);
+
+        add = findViewById(R.id.buttonAdd);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TransActivity.this, MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/work/homebookkeeping/WalletActivity.java
+++ app/src/main/java/com/work/homebookkeeping/WalletActivity.java
@@ -1,14 +1,28 @@
 package com.work.homebookkeeping;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WalletActivity extends AppCompatActivity {
 
+    Button add;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_wallet);
+
+        add = findViewById(R.id.buttonAdd);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WalletActivity.this, MainActivity.class));
+            }
+        });
     }
 }
