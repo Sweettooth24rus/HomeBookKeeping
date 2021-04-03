aa4277e1c98a2a69efe236bc8c13236a80a25ed2
Konstantin <Sweettooth24rus>
2021-04-03 19:25:31 +0700

    Скелет
    Добавил необходимые активности

--- app/src/main/java/com/work/homebookkeeping/MainActivity.java
+++ app/src/main/java/com/work/homebookkeeping/MainActivity.java
@@ -2,13 +2,48 @@ package com.work.homebookkeeping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton options;
+    Button trans;
+    ImageButton wallet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options = findViewById(R.id.imageButtonOptions);
+        trans = findViewById(R.id.buttonTrans);
+        wallet = findViewById(R.id.imageButtonAddWallet);
+
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+            }
+        });
+
+        trans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TransActivity.class));
+            }
+        });
+
+        wallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, WalletActivity.class));
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/work/homebookkeeping/TransActivity.java
@@ -0,0 +1,14 @@
+package com.work.homebookkeeping;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TransActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_trans);
+    }
+}
